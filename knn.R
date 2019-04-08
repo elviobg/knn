ea0779dc40de51b3ba5d24c92ee69e2854a29ea7
Elvio Gomes <elviobg@gmail.com>
2019-04-08 11:08:26 -0300

    Implements knn classification
    Implements knn classifier with Mode function

--- knn.R
+++ knn.R
@@ -5,10 +5,19 @@ distance <- function(vA, vB){
   sqrt(s)
 }
 
+mode <- function(v){
+  sort(table(v), decreasing=T)[1]
+}
+
 knn <- function(data, labels, instance, k){
   distances <- c()
   for(i in 1:nrow(data)){
     distances <- c(distances, distance(instance, data[i,]))
   }
   k_nearests <- labels[sort.list(distances)[1:k]]
+}
+
+knn_classifier <- function(data, labels, instance, k){
+  k_nearests <- knn(data, labels, instance, k)
+  mode(k_nearests)
 }
