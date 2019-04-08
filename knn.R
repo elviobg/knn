34b03c2e64150f37db3e2a4e764215efae45947f
Elvio Gomes <elviobg@gmail.com>
2019-04-08 11:06:11 -0300

    implements knn
    Implementation to knn

--- knn.R
+++ knn.R
@@ -4,3 +4,11 @@ distance <- function(vA, vB){
   s =sum(e)
   sqrt(s)
 }
+
+knn <- function(data, labels, instance, k){
+  distances <- c()
+  for(i in 1:nrow(data)){
+    distances <- c(distances, distance(instance, data[i,]))
+  }
+  k_nearests <- labels[sort.list(distances)[1:k]]
+}
